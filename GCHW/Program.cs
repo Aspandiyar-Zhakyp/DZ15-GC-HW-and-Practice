0637ea6f7d922959ff384190ec980dad3d0fbfb5
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-17 11:44:05 +0600

    DZ15-GC-hw

---
+++ GCHW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace GCHW
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+            var consoleType = typeof(Console);
+        foreach (var type in consoleType.Assembly.GetTypes())
+            {
+            if (type.Name == "Console")
+            {
+                if (type.IsClass)
+                {
+                    foreach (var method in type.GetMethods())
+                    {
+                        if (method is MethodInfo)
+                        {
+                                Console.WriteLine(method);
+                        }
+                    }
+                }
+            }
+       }
+
+            var randomClass = new Random() { Id = 1, name = "Click", isRandom = false };
+            foreach (var type in randomClass.GetType().Assembly.GetTypes())
+            {
+            if (type.IsClass)
+            {
+                foreach (var property in type.GetProperties())
+                {
+                    if (property is PropertyInfo)
+                    {
+                            Console.WriteLine($"{property.Name} = {property.GetValue(randomClass)}");
+                    }
+                }
+            }
+            }
+        }
+    }
+}
---
+++ GCHW/Random.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCHW
+{
+    public class Random
+    {
+        public int Id { get; set; }
+        public bool isRandom { get; set; }
+        public string name { get; set; }
+    }
+}
